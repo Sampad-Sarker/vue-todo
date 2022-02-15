c8cce58433bfae19eaa247480dcf22826dfa71a3
Sampad-Sarke <sampadsarker777@gmail.com>
2022-02-16 02:10:10 +0600

    filter tasks added

---
+++ src/components/Composer.vue
@@ -0,0 +1,25 @@
+<template>
+    <form @submit.prevent ="$emit('addTask')">
+        <input
+            type="text"
+            placeholder="Type and hit enter..."
+            :value="modelValue"
+            class="composer__input"
+            @keyup="edit"
+            @keypress="$emit('update:modelValue',$event.target.value)"
+        />
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'Composer',
+    emits:['addTask','update:modelValue'],
+
+    props:['modelValue'],
+
+}
+</script>
+
+<style>
+</style>
